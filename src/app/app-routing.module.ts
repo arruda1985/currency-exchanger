48b991711ecfdf3f81a76a7331e0df0843647d21
arruda <arruda.danielc@gmail.com>
2023-02-03 08:10:13 -0300

    Adding empty pages + routing config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailComponent } from './pages/detail/detail.component';
 
 const routes: Routes = [
-]
-
+  { path: 'home', component: HomeComponent },
+  { path: 'detail', component: DetailComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+];
 @NgModule({
   declarations: [],
   imports: [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './pages/home/home.component';
+import { DetailComponent } from './pages/detail/detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
